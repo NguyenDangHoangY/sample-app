f8ee40acd74e2e5ac9adf84c2b73764d34ba338a
y.nguyen <y.nguyen@orientsoftware.com>
2022-08-16 15:15:52 +0700

    edit controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", ""
   end
 
   test "should get help" do
