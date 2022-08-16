82ff1ec102eac00376cbaaa701f40e640d83770c
y.nguyen <y.nguyen@orientsoftware.com>
2022-08-16 15:06:33 +0700

    add link to pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'static_pages#home'
+
+  root'static_pages#home'
+  get'/help', to: 'static_pages#help'
+  get'/about',to: 'static_pages#about'
+  get'/contact',to: 'static_pages#contact'
 end
