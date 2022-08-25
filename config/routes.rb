8c720b3f74f1050a378f562e9fd7749556635cd3
y.nguyen <y.nguyen@orientsoftware.com>
2022-08-25 16:26:10 +0700

    preview

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   get '/logout', to:'sessions#destroy'
 
   resources :users
+  resources :account_activations, only:[:edit]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_021224) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_043511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_021224) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated"
+    t.datetime "activated_at"
   end
 
 end
