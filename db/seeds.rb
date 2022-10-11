7e4a409ef8a1e8b484dd5cf72fe57da672209494
y nguyen <y.nguyen@orientsoftware.com>
2022-10-11 15:35:13 +0700

    bug will paganate

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,20 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Create a main sample user.
-User.create!(name: "Example User",
-            email: "example@railstutorial.org",
-            password: "foobar",
-            password_confirmation: "foobar",
-            admin: true,
-            activated: true,
-            activated_at: Time.zone.now)
+# User.create!(name: "Example User",
+#             email: "example@railstutorial.org",
+#             password: "foobar",
+#             password_confirmation: "foobar",
+#             admin: true,
+#             activated: true,
+#             activated_at: Time.zone.now)
 
             
 # Generate a bunch of additional users.
-99.times do |n|
-    name = Faker::Name.name
-    email = "example-#{n+1}@railstutorial.org"
+10.times do |n|
+    # name = Faker::Name.name
+    name = "name-#{n}"
+    email = "example-#{n+100}@railstutorial.org"
     password = "password"
     User.create!(name: name,
                 email: email,
@@ -30,9 +31,9 @@ User.create!(name: "Example User",
 end
                 
 # Generate microposts for a subset of users.
-users = User.order(:created_at).take(5)
-50.times do
-    content = Faker::Lorem.sentence(word_count: 5)
-    users.each { |user| user.microposts.create!(content: content) }
-end
+# users = User.order(:created_at).take(5)
+# 50.times do
+#     content = Faker::Lorem.sentence(word_count: 5)
+#     users.each { |user| user.microposts.create!(content: content) }
+# end
                 
